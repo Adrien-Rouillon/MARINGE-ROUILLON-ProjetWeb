36d8a34951a0ea6e747461c577ed464544491f8b
Alexis-Maringe <alexis.maringe@efrei.net>
2026-05-09 01:21:35 +0200

    Update carousel image in accueil.html, refactor contact form submission logic, and adjust styles in styles.css

--- javascript/contact.js
+++ javascript/contact.js
@@ -2,44 +2,24 @@ const contactForm = document.querySelector('#contact-form');
 
 if (contactForm) {
     contactForm.addEventListener('submit', (e) => {
-        e.preventDefault(); // On bloque le rechargement immédiat
+        e.preventDefault();
 
-        const res = confirm("Etes-vous sûr de vouloir envoyer le message ?");
+    
+        const nouveauCom = {
+            nom: document.querySelector('#name').value,
+            email: document.querySelector('#email').value,
+            texte: document.querySelector('#message-text').value,
+            date: new Date().toLocaleString()
+        };
 
-        if (res === true) {
-            const name = document.querySelector('#name').value;
-            const email = document.querySelector('#email').value;
-            const message = document.querySelector('#message-text').value;
+ 
+        let liste = JSON.parse(localStorage.getItem('commentaires')) || [];
 
-            // 3. Envoyer au serveur Node
-            fetch('http://localhost:3000/commentaire', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ 
-                    nom: name,
-                    email: email,
-                    texte: message,
-                    date: new Date().toLocaleString() 
-                }),
-            })
-            .then(response => {
-                if (!response.ok) throw new Error('Erreur serveur');
-                return response.text();
-            })
-            .then(data => {
-                alert("Succès : " + data); 
-                contactForm.reset(); // Vide tout le formulaire d'un coup
-            })
-            .catch(error => {
-                console.error('Erreur:', error);
-                alert("Le serveur ne répond pas. Lancez-vous bien 'node serveur.js' ?");
-            });
 
-        } else {
-            // Si l'utilisateur annule
-            alert("Envoi annulé. Vous pouvez continuer à modifier votre message.");
-        }
+        liste.push(nouveauCom);
+        localStorage.setItem('commentaires', JSON.stringify(liste));
+
+        alert("Message enregistré dans le navigateur !");
+        contactForm.reset();
     });
 }
